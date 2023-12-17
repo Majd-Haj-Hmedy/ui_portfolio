ff20dde22d599bcd0bba1abe3f72428f9fe6ade3
Majd Haj Hmidi <majdhajhmidi@gmail.com>
2023-12-17 11:25:48 +0300

    Initialized Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+    );
+  }
+}
